afa64cdccdf9db8e4b365c31d283504c85566982
gotpop <liam@gotpop.net>
2022-11-25 20:16:33 +0000

    Correcting image paths

--- src/data/shop.ts
+++ src/data/shop.ts
@@ -3,24 +3,24 @@ export const shopItems = [
         "id": 1,
         "name": "HTML & JS",
         "price": 10.99,
-        "url": "/src/images/books.png"
+        "url": "/books.png"
     },
     {
         "id": 2,
         "name": "Stunning CSS",
         "price": 19.99,
-        "url": "/src/images/html.png"
+        "url": "/html.png"
     },
     {
         "id": 3,
         "name": "Keyboard",
         "price": 120,
-        "url": "/src/images/keyboard.png"
+        "url": "/keyboard.png"
     },
     {
         "id": 4,
         "name": "MacBook Pro",
         "price": 1200,
-        "url": "/src/images/mac.png"
+        "url": "/mac.png"
     }
 ]
