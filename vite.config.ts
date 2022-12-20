a2c8df32f925d8d84347d2cbb58a0da39c862b65
GotPop <liam@gotpop.net>
2022-12-20 21:01:34 +0000

    `Enhancement:` Adding MSW test

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./src/setup.ts'],
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
     }
