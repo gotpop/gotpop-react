309585c5bcb7b6dacf8f264ae8381b6d2d00a3fd
GotPop <gotpop@users.noreply.github.com>
2022-12-25 03:06:28 +0000

    Deleting unused test page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Route, Routes } from 'react-router-dom'
 
 import { About } from '@pages/About'
 import { Home } from '@pages/Home'
-import LayoutStandard from '@layouts/LayoutStandard'
-import Liam from '@pages/Liam'
 import { MenuProvider } from '@context/MenuContext'
 import { Page404 } from '@pages/Page404'
 import { Shop } from '@pages/Shop'
@@ -19,7 +17,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/about" element={<About />} />
-            <Route path="/liam" element={<Liam />} />
             <Route path="*" element={<Page404 />} />
           </Routes>
       </MenuProvider>
