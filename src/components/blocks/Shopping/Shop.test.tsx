f7be808ee689165b74a9c2fa1b33657dd3899ceb
GotPop <gotpop@users.noreply.github.com>
2022-12-25 04:17:07 +0000

    Adding testing providers

--- src/components/blocks/Shopping/Shop.test.tsx
+++ src/components/blocks/Shopping/Shop.test.tsx
@@ -1,42 +1,34 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import {
   render,
   screen,
-  userEvent,
   waitFor,
-  waitForElementToBeRemoved
 } from '../../../utils/test-utils'
 
-import { MenuProvider } from '@context/MenuContext'
 import Shop from './index'
-import { ShoppingCartProvider } from '@context/ShoppingCartContext'
 import { products } from '../../../mocks/handlers'
 
 const URL = 'https://gotpop-simple-server.up.railway.app'
 
-it('loads and displays greeting', async () => {
-  render(
-    <ShoppingCartProvider>
-      <MenuProvider>
-        <Shop url={URL} />
-      </MenuProvider>
-    </ShoppingCartProvider>
-  )
+describe('Shopping area', () => {
+  it('loads and displays products', async () => {
+    render(<Shop url={URL} />)
 
-  await waitFor(() => {
-    const element = screen.queryByTestId('product')
-    expect(element).toBeDefined()
-  })
-    
-  expect(
-    screen.getByRole('heading', { name: 'Html & CSS', level: 3 })
-  ).toBeDefined()
+    await waitFor(() => {
+      const element = screen.queryByTestId('product')
+      expect(element).toBeDefined()
+    })
 
-  await waitFor(() => {
-    products.forEach((product) => {
-      expect(
-        screen.getByRole('heading', { name: product.name, level: 3 })
-      ).toBeDefined()
+    expect(
+      screen.getByRole('heading', { name: 'Html & CSS', level: 3 })
+    ).toBeDefined()
+
+    await waitFor(() => {
+      products.forEach(product => {
+        expect(
+          screen.getByRole('heading', { name: product.name, level: 3 })
+        ).toBeDefined()
+      })
     })
   })
 })
